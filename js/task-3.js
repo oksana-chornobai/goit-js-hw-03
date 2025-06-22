8925e2c086e58d7031f7d6331c6693f7f8b0b0fb
oksana-chornobai <chernobay.oks@icloud.com>
2025-06-22 23:32:35 +0300

    виконано завдання 1, 2, 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,14 @@
-function getElementWidth(content, padding, border) {
-    const contentValue = parseFloat(content);
-    const paddingValue = parseFloat(padding);
-    const borderValue = parseFloat(border);
-    const result = contentValue + paddingValue * 2 + borderValue * 2;
-    return result;
+function filterArray(numbers, value) {
+    let array1 = [];
+    for (const number of numbers) {
+        if (number > value) {
+            array1.push(number);
+        } 
+    } return array1;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
